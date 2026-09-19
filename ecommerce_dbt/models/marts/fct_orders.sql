ab6c90ed249d3bbfba1ca1279641e41564af36f8
Ignacio Ortuzar <i.ortuzar.d@gmail.com>
2026-09-19 13:04:34 +0930

    finish fct_orders with test

--- ecommerce_dbt/models/marts/fct_orders.sql
+++ ecommerce_dbt/models/marts/fct_orders.sql
@@ -14,12 +14,47 @@ with order_item_totals as (
 ),
 
 join_totals_with_orders as (
-    SELECT
+
+    select
         oit.order_id,
-        oit.to
-        customer_id, 
-        order_date, 
-        order_status,
-        shipping_cost
+        o.customer_id,
+        o.order_date,
+        o.order_status,
+        o.shipping_cost,
+
+        oit.total_gross_amount,
+        oit.total_discount_amount,
+        oit.total_net_amount,
+        oit.complete_total_cost,
+        oit.total_gross_margin
+
+    from order_item_totals as oit
+
+    left join {{ ref('stg_orders') }} as o
+        on oit.order_id = o.order_id
+),
+
+join_orders_total_with_order_payments as (
+
+    select
+        jtwo.*,
+        op.failed_payment_count,
+        op.payment_attempts,
+        op.successful_payment_count,
+        op.payment_successful,
+        op.successful_amount,
+        op.last_payment_date
+
+    from join_totals_with_orders as jtwo
+
+    left join {{ ref('int_order_payments') }} as op
+        on jtwo.order_id = op.order_id
+
 )
+
+
+SELECT 
+    *
+FROM
+    join_orders_total_with_order_payments
 
